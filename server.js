ad25fd59f3745d2ccba7c29be3fc6b49ed8e587f
amgraphics <africamediagraphics@gmail.com>
2024-02-14 15:09:22 +0300

    NewTestDeploy now on git

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const dotenv = require("dotenv")
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.get('/', (req,res)=>{res.send("Test Deeploy has Restarted")})
+
+app.listen(port, ()=>console.log(`Server started on port ${port}`))
+
