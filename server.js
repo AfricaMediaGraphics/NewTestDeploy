79755292ba1673d7c6fe9e2bfc88de5368e9bfe4
amgraphics <africamediagraphics@gmail.com>
2024-02-14 15:43:53 +0300

    server.js updated with message

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const port = process.env.PORT || 5000
 
 const app = express()
 
-app.get('/', (req,res)=>{res.send("Test Deeploy has Restarted")})
+app.set("view engine", "ejs")
+
+app.get('/views', (req,res)=>{res.render("index")})
+
+
+//app running notiification
 
 app.listen(port, ()=>console.log(`Server started on port ${port}`))
 
