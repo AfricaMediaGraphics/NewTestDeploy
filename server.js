183c5a90133fd4e749586c351dcf1ea2adb7b754
amgraphics <africamediagraphics@gmail.com>
2024-04-04 14:45:16 +0300

    updated fetch

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const dotenv = require("dotenv")
+const bodyParser = require("body-parser")
 dotenv.config()
 const port = process.env.PORT || 5000
 
@@ -7,6 +8,10 @@ const app = express()
 
 //view engine
 app.set("view engine", "ejs")
+
+//body parser
+let urlencodedParser = app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
 
 //static files
 app.use(express.static(__dirname+ '/public'))
@@ -17,7 +22,15 @@ app.get('/about', (req,res)=>{res.render("beema")})
 app.get('/info', (req,res)=>{res.render("info", {firstName: "Enter Your Name" })})
 app.get('/contact', (req,res)=>{res.render("contact")})
 
-
+app.post('/about', (req,res)=>{
+    res.render("about", {text:"Your account has been created. Now click select group"})
+    console.log(req.body.email + " "+ "renews on" + " "+req.body.renewDate)
+    //db vars-->
+    //let userNameNew = req.body.email
+    //console.log(userNameNew)
+    //db function-->
+    //console.log("Updated record on database for userNameNew")
+})
 
 
 //app running notiification
