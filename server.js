a8863ad086565eebbc19d182afe6155af9603f87
amgraphics <africamediagraphics@gmail.com>
2024-04-14 10:07:26 +0300

    updated fonts

--- server.js
+++ server.js
@@ -16,14 +16,18 @@ app.use(bodyParser.json())
 //static files
 app.use(express.static(__dirname+ '/public'))
 
+//subscribe page visitor log
+function alive(req,res,next){console.log("Customer just walked in"); next()}
+
 // Routes
-app.get('/', (req,res)=>{res.render("index", {text:"Early Stage. Syndicated. Public Exits. NBO."})})
-app.get('/about', (req,res)=>{res.render("beema")})
+app.get('/',(req,res)=>{res.render("index", {text:"Early Stage. Syndicated. Public Exits. NBO."})})
+app.get('/about', alive, (req,res)=>{res.render("beema")})
 app.get('/info', (req,res)=>{res.render("info", {firstName: "Enter Your Name" })})
 app.get('/contact', (req,res)=>{res.render("contact")})
 
+
 app.post('/about', (req,res)=>{
-    res.render("about", {text:"Your account has been created. Now click select group"})
+  res.render("about", {text:"Your account has been created. Now click select group"})
     console.log(req.body.email + " "+ "renews on" + " "+req.body.renewDate)
     //db vars-->
     //let userNameNew = req.body.email
