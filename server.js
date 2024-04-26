5b2e22b01ef0f6e76ea101cc3fa97b16dd522317
amgraphics <africamediagraphics@gmail.com>
2024-04-26 14:25:28 +0300

    friday

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express")
-const dotenv = require("dotenv")
 const connectToDB = require("./config/db")
 const User = require("./user")
+
+const dotenv = require("dotenv")
 const bodyParser = require("body-parser")
 dotenv.config()
 const port = process.env.PORT || 5000
 
 const app = express()
+connectToDB()
 
 //view engine
 app.set("view engine", "ejs")
@@ -21,6 +23,7 @@ app.use(express.static(__dirname+ '/public'))
 //subscribe page visitor log
 function alive(req,res,next){console.log("Customer just walked in"); next()}
 
+
 //Get Routes
 app.get('/',(req,res)=>{res.render("index", {text:"Early Stage. Syndicated. Public Exits. NBO."})})
 app.get('/about', alive, (req,res)=>{res.render("beema")})
@@ -28,41 +31,41 @@ app.get('/info', (req,res)=>{res.render("info", {firstName: "Enter Your Name" })
 app.get('/contact', (req,res)=>{res.render("contact")})
 app.get('/groupConnect', (req,res)=>{res.render("groupConnect")})
 
+
+
+
+
 //Post Routes
 app.post('/about', (req,res)=>{
   res.render("about", {text:"Your account has been created. Now click select group"})
     console.log(req.body.email + " "+ "renews on" + " "+req.body.renewDate)
-   
-   //db code***
+  
+    //db code***
 
   let frontEmail = req.body.email
   let frontName = req.body.renewDate
 
-//DB Actions
-run()
-async function run(){
-    try{
+  console.log(frontEmail + " for the DB " + frontName)
+   
+  //DB Actions
+    run()
+    async function run(){
+      try{
         const user = await User.create({
-            name:frontName, 
-            age:"38",
-            email:frontEmail,
-            city:"Namibia",
-            hobbies:["rolling", "strolling", "mingling"]
+           name:frontName, 
+           age:"38",
+           email:frontEmail,
+           city:"Namibia",
+           hobbies:["Alliance", "JDM"]
         })
+      
         app.get('/', (req,res)=>{res.send("you are now registered as: " + user.name+" "+"and email: " + user.email)})
         console.log(user)
-    }catch(e){
-        console.log(e.message)
-    }
-   
-}
+      }catch(e){
+      console.log(e.message)
+      }
 
-  console.log(frontEmail + " for the DB" + frontName)
-    //db vars-->
-    //let userNameNew = req.body.email
-    //console.log(userNameNew)
-    //db function-->
-    //console.log("Updated record on database for userNameNew")
+      }
 })
 
 
