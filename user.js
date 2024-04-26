cd2c65aa07c9e49564d0103ba3c7b22b5155de8e
amgraphics <africamediagraphics@gmail.com>
2024-04-26 14:24:17 +0300

    user added

---
+++ user.js
@@ -0,0 +1,18 @@
+
+const mongoose=require("mongoose")
+
+const userSchema=new mongoose.Schema({
+    name:String, 
+    age:Number,
+    email:{type:String,required:true},
+    createdAt: Date,
+    udatedAt:Date,
+    bestFriend: mongoose.SchemaTypes.ObjectId,
+hobbies: [String],
+address:{
+    street:String,
+    city:String
+}})
+
+module.exports = mongoose.model("User",userSchema)
+
